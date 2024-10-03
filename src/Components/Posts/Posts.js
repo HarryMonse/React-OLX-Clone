c68ef266a2bc2713ca34d610f6f3c8419d09cddc
Harry Monse <harry.monse1812@gmail.com>
2024-10-03 22:13:58 +0530

    updated

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -65,26 +65,38 @@ function Posts() {
           <span>Fresh recommendations</span>
         </div>
         <div className="cards">
-          <div className="card">
-            <div className="favorite">
-              <Heart></Heart>
-            </div>
-            <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
-            </div>
-            <div className="date">
-              <span>10/5/2021</span>
-            </div>
-          </div>
+          { products.map(product => {
+            return (
+              <div
+                className="card"
+                onClick={() => {
+                  setPostDetails(product);
+                  history.push('/view');
+                }}
+                key={product.id}
+              >
+                <div className="favorite">
+                  <Heart />
+                </div>
+                <div className="image">
+                  <img src={product.url} alt="" />
+                </div>
+                <div className="content">
+                  <p className="rate">&#x20B9; {product.price}</p>
+                  <span className="kilometer">{product.category}</span>
+                  <p className="name"> {product.name}</p>
+                </div>
+                <div className="date">
+                  <span>{product.createdAt}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
 }
+
 
 export default Posts;
